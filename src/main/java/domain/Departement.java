088a7a2331b55f30972ae5a060300b05f59c9b71
nadir inab <nadir.inab.dev@gmail.com>
2023-10-15 21:31:22 +0100

    depratement class is well added

---
+++ src/main/java/domain/Departement.java
@@ -0,0 +1,65 @@
+package domain;
+
+import javax.persistence.*;
+import domain.Employee ;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departement")
+public class Departement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "departement_boss")
+    private String maintenance_date;
+
+    @OneToMany(mappedBy = "departement")
+    private List<Employee> employees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMaintenance_date() {
+        return maintenance_date;
+    }
+
+    public void setMaintenance_date(String maintenance_date) {
+        this.maintenance_date = maintenance_date;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
