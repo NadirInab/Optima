2b2a7457bb9d03e4d766004895a92eed313fb822
nadir inab <nadir.inab.dev@gmail.com>
2023-10-15 21:32:39 +0100

    task is fine and done

---
+++ src/main/java/domain/Task.java
@@ -0,0 +1,75 @@
+package domain;
+
+
+import javax.persistence.*;
+
+public class Task {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "deadline")
+    private String deadline;
+
+    @Column(name = "employee")
+    private String employee;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee assignedTo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Employee getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(Employee assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+}
